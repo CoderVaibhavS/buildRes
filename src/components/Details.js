7ce4d792257c66bd2a89b3c900a300a16cf8cc73
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2023-06-17 20:46:14 +0530

    Commented Work Details Portion

--- src/components/Details.js
+++ src/components/Details.js
@@ -48,22 +48,24 @@ export default function Details(props) {
         })
     }
 
-    const updateWork = (event, idx) => {
-        setWork((work) => {
-            let newWork = [...work];
-            newWork[idx] = event.target.value;
-            return newWork
-        })
-    }
+    // const updateWork = (event, idx) => {
+    //     setWork((work) => {
+    //         let newWork = [...work];
+    //         newWork[idx] = event.target.value;
+    //         return newWork
+    //     })
+    // }
 
-    const updateWorkDet = (event, idx, a) => {
-        setWork((work) => {
-            let newWork = [...work];
-            // let a = event.target.name;
-            newWork[idx].a = event.target.value;
-            return newWork
-        })
-    }
+    // const updateWorkDet = (event, idx, a) => {
+    //     setWork((work) => {
+    //         let newWork = [...work];
+    //         // let a = event.target.name;
+    //         if(a === 'role') {
+    //             newWork[idx].c_name = event.target.value;
+    //         }
+    //         return newWork
+    //     })
+    // }
 
     const addSkill = () => {
         setSkills((skills) => [...skills, ""]);
@@ -172,10 +174,10 @@ export default function Details(props) {
             <button type='button' className='add' onClick={addWork}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M384 32C419.3 32 448 60.65 448 96V416C448 451.3 419.3 480 384 480H64C28.65 480 0 451.3 0 416V96C0 60.65 28.65 32 64 32H384zM224 368C237.3 368 248 357.3 248 344V280H312C325.3 280 336 269.3 336 256C336 242.7 325.3 232 312 232H248V168C248 154.7 237.3 144 224 144C210.7 144 200 154.7 200 168V232H136C122.7 232 112 242.7 112 256C112 269.3 122.7 280 136 280H200V344C200 357.3 210.7 368 224 368z" /></svg></button>
 
             {work.map((work_det, idx) =>
-                <form value={work_det} key={idx} onChange={(evt) => { updateWork(evt, idx) }}>
+                <form value={work_det} key={idx}>
                     <div className='form_div'>
                         <label htmlFor="">Company Name</label>
-                        <input type="text" placeholder='Company Name' name="role" value={work_det.role} onChange={(evt) => { updateWorkDet(evt, idx, 'role') }} />
+                        <input type="text" placeholder='Company Name' name="role" />
                     </div>
 
                     <div className='form_div'>
